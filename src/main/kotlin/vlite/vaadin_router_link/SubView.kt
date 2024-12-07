8b66497aebbfe92d23c4dfcf8826a8ea359473a0
Vitalii Lipovetskii <vlipovetskii@users.noreply.github.com>
2024-12-07 16:44:25 +0200

    + routerLink(icon = VaadinIcon.PAPERPLANE, text = "SubView", viewType = SubView::class)

--- src/main/kotlin/vlite/vaadin_router_link/SubView.kt
+++ src/main/kotlin/vlite/vaadin_router_link/SubView.kt
@@ -1,17 +1,15 @@
 package vlite.vaadin_router_link
 
 import com.github.mvysny.karibudsl.v10.span
-import com.vaadin.flow.component.icon.VaadinIcon
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.router.Route
 
 @Suppress("unused")
-@Route("/")
-class MainView : VerticalLayout() {
+@Route("/sub")
+class SubView : VerticalLayout() {
 
     init {
-        span("MainView")
-        routerLink(icon = VaadinIcon.PAPERPLANE, text = "SubView", viewType = SubView::class)
+        span("SubView")
     }
 
 }
